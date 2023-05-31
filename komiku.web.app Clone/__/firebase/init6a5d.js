356fdaf8945696699f757c85f9ea9066dccf6a11
Kang Sura <130182449+Kang-Sura@users.noreply.github.com>
2023-05-31 16:31:14 +0800

    Create init6a5d.js

---
+++ komiku.web.app Clone/__/firebase/init6a5d.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyBM7DrpF4u_sDjPQ61NroxkuUPGubiaJN0",
+  "appId": "1:319275463756:web:2c4c58119108a82b6eb0e9",
+  "authDomain": "komiku-co-id.firebaseapp.com",
+  "databaseURL": "https://komiku-co-id.firebaseio.com",
+  "measurementId": "G-J4S9ZWQ489",
+  "messagingSenderId": "319275463756",
+  "projectId": "komiku-co-id",
+  "storageBucket": "komiku-co-id.appspot.com"
+});
