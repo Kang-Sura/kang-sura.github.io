1ff7f8c71288bffcdebff72e41322dcb9de4f4d9
Kang Sura <130182449+Kang-Sura@users.noreply.github.com>
2025-05-22 14:34:42 +0800

    Update script.js

--- komik-uploader/script.js
+++ komik-uploader/script.js
@@ -1,5 +1,5 @@
 const clientId = '681912570121-tge4lb5enkhrvtd8tak2s2um7mcl4uoh.apps.googleusercontent.com';
-const redirectUri = 'http://localhost:8000';
+const redirectUri = 'http://localhost/';
 const scope = 'https://www.googleapis.com/auth/blogger https://www.googleapis.com/auth/drive.file';
 let token = null;
 
